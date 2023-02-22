46112965a5349972073ef483f12ea4197b22ef2e
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2023-02-23 07:36:32 +0900

    Add project src-layout

---
+++ src/gimix/__main__.py
@@ -0,0 +1,8 @@
+import sys
+
+def main() -> None:
+    sys.stdout.write("gimix!")
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
---
+++ tests/gimix/test___init__.py
@@ -0,0 +1,4 @@
+import gimix as mix
+
+def test_version():
+    assert mix.__version__ == "0.0.1"
