0ccc3ad4aa21f93b24887e3f8975385da95cfc04
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2023-02-23 19:37:00 +0900

    Add file load

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from _pytest._py.path import LocalPath
+import pytest
+import json
+
+
+@pytest.fixture(scope="session")
+def tmpdir(tmpdir_factory) -> LocalPath:
+    tmpdir = tmpdir_factory.mktemp("gimix")
+    return tmpdir
+
+
+@pytest.fixture(scope="session")
+def text_file(tmpdir) -> str:
+    text_file = str(tmpdir.join("test.txt"))
+    with open(text_file, mode="w", encoding="utf-8") as file:
+        file.write("abc")
+    return text_file
+
+
+@pytest.fixture(scope="session")
+def json_file(tmpdir) -> str:
+    json_file = str(tmpdir.join("test.json"))
+    contents = {
+        "key" : "value",
+        "key_list" : [1, 2, 3],
+        "key_dict" : {"a": 1, "b": 2, "c": "123"},
+    }
+    with open(json_file, mode="w", encoding="utf-8") as file:
+        json.dump(contents, file)
+    return json_file
---
+++ tests/gimix/files/test___init__.py
@@ -0,0 +1,14 @@
+from gimix import files
+import pytest
+
+
+def test_load_text(text_file):
+    contents = files.load(text_file)
+    assert contents[0] == "abc"
+
+
+def test_load_json(json_file):
+    contents = files.load(json_file)
+    assert contents.get("key") == "value"
+    assert contents.get("key_list") == [1, 2, 3]
+    assert contents.get("key_dict") == {"a": 1, "b": 2, "c": "123"}
