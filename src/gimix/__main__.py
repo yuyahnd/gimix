accaa9494753ef9a7a7f497252899623ae73f5be
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2023-02-24 22:55:46 +0900

    Add mix load command

--- src/gimix/__main__.py
+++ src/gimix/__main__.py
@@ -1,8 +1,21 @@
-import sys
+from argparse import ArgumentParser
+from gimix.cmd import load
+
 
 def main() -> None:
-    sys.stdout.write("gimix!")
-    sys.exit()
+    """gimix main."""
+    parser = ArgumentParser(description='mix commands')
+    subparsers = parser.add_subparsers()
+
+    # sub commands
+    load.parser(subparsers)
+
+    # handle command
+    args = parser.parse_args()
+    if hasattr(args, 'handler'):
+        args.handler(args)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
---
+++ src/gimix/cmd/load.py
@@ -0,0 +1,49 @@
+from typing import Any
+from argparse import (
+    Namespace,
+    _SubParsersAction as SubParsersAction,
+)
+from pathlib import Path
+from gimix import files
+import sys
+
+
+def parser(subparser: SubParsersAction) -> None:
+    parser  = subparser.add_parser("load", help="see `load -h`")
+    parser.add_argument("-i", "--input",
+                        type=str,
+                        help="file path")
+    parser.add_argument("-m", "--mode",
+                        type=str,
+                        choices=["r", "rb"],
+                        default="r",
+                        help="file open mode")
+    parser.add_argument("-e", "--encoding",
+                        type=str,
+                        default="utf-8",
+                        help="file encoding")
+    parser.set_defaults(handler=load_command)
+
+
+def load_command(args: Namespace) -> None:
+    input = args.input
+    mode = args.mode
+    encoding = args.encoding
+
+    if sys.stdin.isatty():
+        contents = files.load(input, mode, encoding)
+        echo(contents)
+    else:
+        paths = []
+        for input in sys.stdin:
+            path = Path(input.rstrip())
+            if path.is_file():
+                paths.append(str(path))
+
+        for input in paths:
+            contents = files.load(input, mode, encoding)
+            echo(contents)
+
+def echo(contents: Any) -> None:
+    if isinstance(contents, list):
+        sys.stdout.write("".join(contents))
